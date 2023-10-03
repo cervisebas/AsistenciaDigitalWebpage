da2469bb72a43aead8bb71fd0a6b998695ace69b
Sebastian Cerviño <cervisebas@gmail.com>
2023-10-03 11:01:44 -0300

    03/10/2023 11:01hs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+/// <reference types="vite-plugin-svgr/client" />
 import React from 'react';
+import Icon from './assets/icon.webp';
 import './App.scss';
 
 export default React.memo(function App() {
   return(<div id={'principal-content'}>
-    
+    <div className={'title'}>
+      <img src={Icon} alt={'Icono'} className={'icon'} />
+      <button>Empezar</button>
+    </div>
   </div>);
 })
